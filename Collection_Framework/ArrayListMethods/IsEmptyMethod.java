59745f07a3c53f905f316227878d56208944f058
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-23 22:05:18 +0530

    check ArrayList is Empty Or Not

---
+++ Collection_Framework/ArrayListMethods/IsEmptyMethod.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+
+public class IsEmptyMethod {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        System.out.println(al.isEmpty());
+    }
+}
