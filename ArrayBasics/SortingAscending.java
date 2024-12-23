1c9060fb8f78dad16e474d03f546c2746dd9ba06
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-23 21:59:23 +0530

    Sorting Array in Ascending Order

---
+++ ArrayBasics/SortingAscending.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SortingAscending {
+    public static void printArray(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void sortAsc(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i+1; j < a.length; j++) {
+                if(a[i]>a[j]){
+                    swap(a,i,j);
+                }
+            }
+        }
+        printArray(a);
+    }
+    public static void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of Array: ");
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        sortAsc(a);
+    }
+}
