0b66b05c0bd54c756a4aa79c9d0022a9a1276582
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-23 22:16:05 +0530

    Return Length Of ArrayList

---
+++ Collection_Framework/ArrayListMethods/SizeMethod.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class SizeMethod {
+    public static void main(String[] args) {
+        ArrayList<Integer> al = new ArrayList<>();
+        al.add(30);
+        al.add(3);
+        al.add(40);
+        al.add(5);
+        al.add(80);
+        System.out.println(al.size());
+
+    }
+}
