33605824935b1ed58e41c65df70e701df60555c3
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-23 21:42:11 +0530

    Polymorphism

---
+++ OOPS/Polymorphism1.java
@@ -0,0 +1,41 @@
+class Car{
+    String model;
+    int year;
+    int price;
+    Car(String model,int year,int price){
+        this.model=model;
+        this.year=year;
+        this.price=price;
+    }
+    void carDetails(){
+
+    }
+}
+class MarutiSuzuki extends Car{
+    MarutiSuzuki(){
+        super("Swift", 2014, 700000);
+    }
+    void carDetails(){
+        System.out.println("<----Maruti Cars---->");
+        System.out.println("Model: "+model+"\nYear: "+year+"\nPrice: "+price);
+    }
+}
+class Tata extends Car{
+    Tata(){
+        super("Nexon",2020,800000);
+    }
+    void carDetails(){
+        System.out.println("<----Tata Cars---->");
+        System.out.println("Model: "+model+"\nYear: "+year+"\nPrice: "+price);
+    }
+}
+public class Polymorphism1 {
+    public static void main(String[] args) {
+        Car maruti = new MarutiSuzuki();
+        maruti.carDetails();
+        Car tata = new Tata();
+        tata.carDetails();
+    }
+}
+// 2. Design a class `Car` with properties like `model`, `year`, and `price`. 
+// Implement a method to compare two cars based on their price using method overriding.
