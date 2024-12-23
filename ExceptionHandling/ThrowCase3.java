3a42f7541403f205300029a85610652445784970
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-23 22:43:08 +0530

    Throw Case 3

---
+++ ExceptionHandling/ThrowCase3.java
@@ -0,0 +1,7 @@
+public class ThrowCase3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        throw new ArithmeticException("Arithmethic");
+        System.out.println("Good Morning");
+    }
+}
