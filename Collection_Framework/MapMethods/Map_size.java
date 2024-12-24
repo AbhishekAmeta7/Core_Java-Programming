6e0664280f66e4b945789cde2167478de99a4c10
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 23:37:52 +0530

    rerurn the size of Map

---
+++ Collection_Framework/MapMethods/Map_size.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Map_size {
+    public static void main(String[] args) {
+        Map<Integer,String> m = new HashMap<>();
+        m.put(1, "Hello");
+        m.put(2, "Hiii");
+        m.put(4, "bye");
+        m.put(3, "Hey");
+        System.out.println(m.size());
+    }
+}
