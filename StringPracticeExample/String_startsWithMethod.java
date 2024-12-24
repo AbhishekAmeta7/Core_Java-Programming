908c2fff648259336c2273f828611be100363d2d
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:48:44 +0530

    Checks whether a string starts with specified characters

---
+++ StringPracticeExample/String_startsWithMethod.java
@@ -0,0 +1,8 @@
+public class String_startsWithMethod {
+    public static void main(String[] args) {
+        String str = "Java Programming";
+        System.out.println(str.startsWith("Java"));
+        System.out.println(str.startsWith("java")); 
+        System.out.println(str.startsWith("Programming")); 
+    }
+}
