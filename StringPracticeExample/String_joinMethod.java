2edcbdf0f3ef65eb966db4b11959badfe790a564
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:43:31 +0530

    Join On or More String With Specified Separator

---
+++ StringPracticeExample/String_joinMethod.java
@@ -0,0 +1,7 @@
+public class String_joinMethod {
+    public static void main(String[] args) {
+        String[] s = {"Java", "is", "fun"};
+        String s1 = String.join(" ", s);
+        System.out.println(s1);
+    }
+}
