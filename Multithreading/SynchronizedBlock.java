3dfa00eff3930a3a51777a1dc2cc31ef2b61024d
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 13:08:17 +0530

    Synchronized Block in java

---
+++ Multithreading/SynchronizedBlock.java
@@ -0,0 +1,42 @@
+class A {
+    public void wish(int n) {
+        synchronized(this) {
+            for (int i = 1; i <= n; i++) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    
+                }
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+class B extends Thread {
+    A a;
+    B(A a) {
+        this.a = a;
+    }
+    public void run() {
+        a.wish(10);
+    }
+}
+class C extends Thread {
+    A a;
+    C(A a) {
+        this.a = a;
+    }
+    public void run() {
+        a.wish(5);
+    }
+}
+public class SynchronizedBlock {
+    public static void main(String[] args) {
+        A s = new A();
+        B b = new B(s);
+        C c = new C(s);
+        b.start();
+        c.start();
+    }
+}
