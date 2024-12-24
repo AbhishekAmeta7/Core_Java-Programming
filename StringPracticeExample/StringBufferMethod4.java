5ef9e48d930f9ca17236c75207332731ead747ff
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:54:51 +0530

    insert() in StringBuffer

---
+++ StringPracticeExample/StringBufferMethod4.java
@@ -0,0 +1,7 @@
+public class StringBufferMethod4 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.insert(5, " Java");
+        System.out.println(sb);
+    }
+}
