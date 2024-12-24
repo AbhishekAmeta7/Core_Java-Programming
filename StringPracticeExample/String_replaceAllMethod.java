2e61b9c28d8b9de63451c6bf5b5b1dad9d8719f5
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:46:30 +0530

    Replaces each substring of this string that matches the given regular expression with the given replacement

---
+++ StringPracticeExample/String_replaceAllMethod.java
@@ -0,0 +1,7 @@
+public class String_replaceAllMethod {
+    public static void main(String[] args) {
+        String s = "Python is programming language";
+        String s1 = s.replaceAll("Python", "Java");
+        System.out.println(s1);
+    }
+}
