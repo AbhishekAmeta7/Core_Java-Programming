cf29200d14ea74f07239a70c0bade64a3ae04f3a
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:41:39 +0530

    Return First Occurance OF Specified String

---
+++ StringPracticeExample/String_indexOfMethod.java
@@ -0,0 +1,6 @@
+public class String_indexOfMethod {
+    public static void main(String[] args) {
+        String s = "Java is Programming Language";
+        System.out.println(s.indexOf('P'));
+    }
+}
