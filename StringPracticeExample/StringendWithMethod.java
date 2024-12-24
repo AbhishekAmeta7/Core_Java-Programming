3979ddeef26b1b791276b3091cda882ad38781ff
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:34:59 +0530

    String end With Specified Suffix

---
+++ StringPracticeExample/StringendWithMethod.java
@@ -0,0 +1,8 @@
+public class StringendWithMethod {
+    public static void main(String[] args) {
+        String s1 = "Hello World";
+        String s2 = "Java Programming";
+        System.out.println(s1.endsWith("World")); 
+        System.out.println(s2.endsWith("ing"));  
+    }
+}
