ac8e6bb35b732487bbee71d8124b24992f0b4689
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:49:18 +0530

    Returns a new string which is the substring of a specified string

---
+++ StringPracticeExample/String_substringMethod.java
@@ -0,0 +1,6 @@
+public class String_substringMethod {
+    public static void main(String[] args) {
+        String s = "Java is Programming Language";
+        System.out.println(s.substring(8,19));
+    }
+}
