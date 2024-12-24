7f1b6c3f92ed3da38b8e933cb3d66bc9902fcde5
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:51:26 +0530

    Return Thr String Representation Of The Spefied Value

---
+++ StringPracticeExample/String_valueOfMethod.java
@@ -0,0 +1,8 @@
+public class String_valueOfMethod {
+    public static void main(String[] args) {
+        int n = 123;
+        String s = String.valueOf(n);
+        System.out.println("Integer as String: " + s);
+        
+    }
+}
