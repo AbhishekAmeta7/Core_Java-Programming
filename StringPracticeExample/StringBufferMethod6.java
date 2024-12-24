33f62a77004201dc1e2a95d5bbcbab9d1f7de9af
AbhishekAmeta7 <abhishekameta55@gmail.com>
2024-12-24 12:56:09 +0530

    Set Character At Given Position in The String

---
+++ StringPracticeExample/StringBufferMethod6.java
@@ -0,0 +1,7 @@
+public class StringBufferMethod6 {
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer("Hello");
+        sb.setCharAt(1, 'a');
+        System.out.println(sb);
+    }
+}
