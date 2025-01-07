aae96079efae5c3d66ba2c4b61e18b1854932155
AbhishekAmeta7 <abhishekameta55@gmail.com>
2025-01-07 10:27:19 +0530

    ArrayDeque in java

---
+++ Collection_Framework/ArrayDeque/ArrayDequeEx1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ArrayDequeEx1 {
+    public static void main(String[] args) {
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        ad.add(4);
+        ad.add(2);
+        ad.add(4);
+        ad.add(5);
+        ad.add(3);
+
+        System.out.println(ad);
+
+    }
+}
