ba05189241e7cf9f48f98a21645b6341a80209fb
AbhishekAmeta7 <abhishekameta55@gmail.com>
2025-01-08 16:46:28 +0530

    Remove an element from the tail of the queue.

---
+++ Collection_Framework/ArrayDeque/ArrayDeque_Methods/RemoveLast.java
@@ -0,0 +1,23 @@
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class RemoveLast {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of ArrayDeque: ");
+        int n = sc.nextInt();
+        ArrayDeque<Integer> ad = new ArrayDeque<>();
+        for (int i = 1; i <= n; i++) {
+            int s= sc.nextInt();
+            ad.add(s);
+        }
+        for (Integer s : ad) {
+            System.out.print(s+" ");
+        }
+        System.out.println();
+        ad.removeLast();
+        for (Integer i : ad) {
+            System.out.print(i+" ");
+        }
+    }
+}
