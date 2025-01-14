26d139bc1c482fbba7f5ef1f1ca82977c6a0f5bb
AbhishekAmeta7 <abhishekameta55@gmail.com>
2025-01-14 20:50:09 +0530

    Find Palindrome From Stack

---
+++ Collection_Framework/Stack/PalindromeStack.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class PalindromeStack {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of Stack: ");
+        int n = sc.nextInt();
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            s.push(x);
+        }
+        for (int i = 0; i < s.size(); i++) {
+            int ans = 0,temp = s.get(i);
+            while(temp>0) {
+                int a = temp%10;
+                ans = ans*10+a;
+                temp /= 10;
+            }
+            if(s.get(i)==ans){
+                System.out.print(ans+" ");
+            }
+        }
+    }
+}
