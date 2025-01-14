c5b8d83fcb39ac2e13503317b61b79b20846dfb4
AbhishekAmeta7 <abhishekameta55@gmail.com>
2025-01-14 20:39:12 +0530

    Find Even From Stack

---
+++ Collection_Framework/Stack/EvenFindStack.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class EvenFindStack{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Size Of Stack: ");
+        int n = sc.nextInt();
+        Stack<Integer> s = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            s.push(x);
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if(s.get(i)%2 == 0) {
+                System.out.print(s.get(i)+" ");
+            }
+        }
+    }
+}
